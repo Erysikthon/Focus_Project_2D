5402c2cfe596bf41e0d84706ac24a70e87383df6
CodingDeni-boop <dbugatti@ethz.ch>
2026-04-30 11:44:38 +0200

    TCNN2

--- TCNN/TCNN_model.py
+++ TCNN/TCNN_model.py
@@ -36,53 +36,51 @@ class TCNN(Module):
             BatchNorm3d(32),
             Conv3d(32, 32, (5, 5, 5), (1, 1, 1), (2, 2, 2)), 
             BatchNorm3d(32), 
+            Dropout(0.1),
             MaxPool3d((1, 2, 2), (1, 2, 2)),
             AvgPool3d((2, 1, 1), (2, 1, 1)),
             ReLU()
         )
-        self.block1 = Sequential(*[ResBlock(32) for whatever in range(0, 6)])
+        self.block1 = Sequential(*[ResBlock(32) for whatever in range(0, 3)])
         self.transition_12 = Sequential(
             Conv3d(32, 48, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(48),
             Conv3d(48, 48, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(48), 
+            Dropout(0.1),
             MaxPool3d((1, 2, 2), (1, 2, 2)),
             AvgPool3d((2, 1, 1), (2, 1, 1)),
             ReLU()
         )
-        self.block2 = Sequential(*[ResBlock(48) for whatever in range(0, 6)])
+        self.block2 = Sequential(*[ResBlock(48) for whatever in range(0, 3)])
         self.transition_23 = Sequential(
             Conv3d(48, 64, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(64),
             Conv3d(64, 64, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(64), 
             MaxPool3d((1, 2, 2), (1, 2, 2)),
-            ReLU()
+            ReLU(),
+            Dropout(0.2)
         )
-        self.block3 = Sequential(*[ResBlock(64) for whatever in range(0, 6)])
+        self.block3 = Sequential(*[ResBlock(64) for whatever in range(0, 3)])
         self.final_convolution = Sequential(
             Conv3d(64, 64, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(64),
             Conv3d(64, 64, (3, 3, 3), (1, 1, 1), (1, 1, 1)), 
             BatchNorm3d(64), 
-            ReLU()
+            ReLU(),
+            Dropout(0.3)
         )
         self.time_head = Sequential(
-            Linear(2048, 724), 
-            ReLU(),
-            Dropout(0.3),
-            Linear(724, 256), 
-            ReLU(), 
+            Linear(64, 128),
+            GELU(),
             Dropout(0.3)
         )
         self.final_head = Sequential(
-            Linear(1024, 512), 
-            ReLU(),
+            Linear(128, 64), 
+            ReLU(), 
             Dropout(0.3),
-            Linear(512, 128),
-            ReLU(),
-            Dropout(0.3),
-            Linear(128, 5)
+            Linear(64, 5)
         )
 
     def forward(self, x : Tensor):
@@ -102,14 +100,25 @@ class TCNN(Module):
         x = self.final_convolution(x)
         
         B, C, T, H, W = x.shape
-        x = x.permute(0, 2, 1, 3, 4)
-        x = x.reshape(B*T, C*H*W)
-        x = self.time_head(x)        # [B*T, 70]
-        x = x.view(B, T, -1)
+        # --- spatial global average pooling ---
+        x = x.mean(dim=(3, 4))          # [B, C, T]
 
-        x = x.reshape(B, -1)       # [B, T*70]
-        x = self.final_head(x)
-        
+        # --- prepare per-time-step features ---
+        x = x.permute(0, 2, 1)          # [B, T, C]
+        x = x.reshape(B * T, C)         # [B*T, C]
+
+        # --- time head ---
+        x = self.time_head(x)           # [B*T, D]
+
+        # --- restore temporal structure ---
+        x = x.view(B, T, -1)            # [B, T, D]
+
+        # --- temporal pooling (recommended) ---
+        x = x.mean(dim=1)               # [B, D]
+
+        # --- final classification ---
+        x = self.final_head(x)          # [B, num_classes]
+
         return x
 
 def train_model(dataloaders : Sequence[DataLoader], network : TCNN, loss_fn : CrossEntropyLoss, optimizer : torch.optim.AdamW, device: torch.device):
